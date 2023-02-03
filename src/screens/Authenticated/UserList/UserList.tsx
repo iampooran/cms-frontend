f941182ff34e942b70f72e5ab3bef5d0f1981409
iampooran <pooransen50@gmail.com>
2023-02-03 09:02:10 +0530

    UserList Changes

--- src/screens/Authenticated/UserList/UserList.tsx
+++ src/screens/Authenticated/UserList/UserList.tsx
@@ -9,6 +9,7 @@ import ToggleOnIcon from "@mui/icons-material/ToggleOn";
 import { Button } from "@mui/material";
 import { ButtonProps } from "@mui/material/Button";
 import { Link } from "react-router-dom";
+import "./UserList.scss"
 
 const AddUserButton = styled(Button)<ButtonProps>(({ theme }) => ({
   color: "white",
@@ -23,134 +24,86 @@ export default function UserList() {
 
   return (
     <>
-      {/* <Box
+    <Box
         sx={{
           my: 8,
           mx: 4,
           display: "flex",
           flexDirection: "column",
-          alignItems: "left",
+          alignItems: "center",
           
         }}
       >
-         
-        <Grid container spacing={1} >
-          <Grid item xs={12} sm={6} >
-            <Typography
-              variant="h6"
-              sx={{ fontWeight: "bold", color: "#BA9B37" }}
-            >
-              Customer Listing
-            </Typography>
-          </Grid>
-
-          <Grid item xs={12} sm={2}>
-            <AddUserButton variant="contained">Add User</AddUserButton>
-          </Grid>
-        </Grid>
-        <Grid container spacing={1}>
-          <Grid item xs={12} sm={6}>
-            <Typography sx={{ color: "black" }} mt={3}>
-              UserName Here
-            </Typography>
-          </Grid>
-
-          <Grid item xs={12} sm={2}>
-            <Typography sx={{ color: "#8D8D8E" }} mt={3}>
-              <ToggleOnIcon />
-
-              <EditIcon sx={{ marginLeft: "25px" }} />
-
-              <DeleteIcon sx={{ marginLeft: "25px" }} />
-            </Typography>
-          </Grid>
-        </Grid>
 
 
-      </Box> */}
-      <Box
-        sx={{
-          my: 8,
-          mx: 4,
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "left",
-          
-        }}
-      >
-        <Grid container >
-          <Grid item xs={12} sm={11} >
-            <Typography
+<div className="userList2">
+        <div className="left">
+        <Typography
             mb={3}
             ml={2}
-              variant="h6"
+              variant="h5"
               sx={{ fontWeight: "bold", color: "#BA9B37" }}
             >
               Customer Listing
             </Typography>
-          </Grid>
-
-          <Grid item xs={12} sm={1}>
-            <AddUserButton variant="contained">Add User</AddUserButton>
-          </Grid>
-        </Grid>
-      <div className="w3-container">
-      <ul className="w3-ul w3-card-4">
-          <li className="w3-bar">
-            <span className="w3-right">
-            <Grid item xs={12} sm={2}>
-            <Typography sx={{ color: "#8D8D8E" }} mt={3}>
-              <Link to="#"><ToggleOnIcon /></Link>
-              <Link to="#"><EditIcon sx={{ marginLeft: "25px" }} /></Link>
-              <Link to="#"><DeleteIcon sx={{ marginLeft: "25px",marginRight: "25px" }} /></Link>
-            </Typography>
-          </Grid>
-            </span>
-            <div className="w3-bar-item">
-              <span className="w3-large">UserName</span>
-              <br />
-              <span>Role</span>
-            </div>
-          </li>
-        </ul>
-        <ul className="w3-ul w3-card-4">
-          <li className="w3-bar">
-            <span className="w3-right">
-            <Grid item xs={12} sm={2}>
-            <Typography sx={{ color: "#8D8D8E" }} mt={3}>
-              <Link to="#"><ToggleOnIcon /></Link>
-              <Link to="#"><EditIcon sx={{ marginLeft: "25px" }} /></Link>
-              <Link to="#"><DeleteIcon sx={{ marginLeft: "25px",marginRight: "25px" }} /></Link>
-            </Typography>
-          </Grid>
-            </span>
-            <div className="w3-bar-item">
-              <span className="w3-large">UserName</span>
-              <br />
-              <span>Role</span>
-            </div>
-          </li>
-        </ul>
-        <ul className="w3-ul w3-card-4">
-          <li className="w3-bar">
-            <span className="w3-right">
-            <Grid item xs={12} sm={2}>
-            <Typography sx={{ color: "#8D8D8E" }} mt={3}>
-              <Link to="#"><ToggleOnIcon /></Link>
-              <Link to="#"><EditIcon sx={{ marginLeft: "25px" }} /></Link>
-              <Link to="#"><DeleteIcon sx={{ marginLeft: "25px",marginRight: "25px" }} /></Link>
-            </Typography>
-          </Grid>
-            </span>
-            <div className="w3-bar-item">
-              <span className="w3-large">UserName</span>
-              <br />
-              <span>Role</span>
-            </div>
-          </li>
-        </ul>
+        </div>
+        <div className="right" >
+        <AddUserButton variant="contained">Add User</AddUserButton> 
+        </div>
       </div>
-      
+
+
+      <div className="userList">
+        <div className="left">
+            <Typography mt={2} mb={2} variant="h6" fontWeight="bold">User Name</Typography>
+        </div>
+        <div className="right">
+        <Typography mt={2} mb={2}>
+              <Link to="#"><ToggleOnIcon /></Link>
+              <Link to="#"><EditIcon sx={{ marginLeft: "25px" }} /></Link>
+              <Link to="#"><DeleteIcon sx={{ marginLeft: "25px",marginRight: "25px" }} /></Link>
+            </Typography>
+        </div>
+      </div>
+
+      <div className="userList">
+        <div className="left">
+            <Typography mt={2} mb={2} variant="h6" fontWeight="bold">User Name</Typography>
+        </div>
+        <div className="right">
+        <Typography mt={2} mb={2}>
+              <Link to="#"><ToggleOnIcon /></Link>
+              <Link to="#"><EditIcon sx={{ marginLeft: "25px" }} /></Link>
+              <Link to="#"><DeleteIcon sx={{ marginLeft: "25px",marginRight: "25px" }} /></Link>
+            </Typography>
+        </div>
+      </div>
+
+      <div className="userList">
+        <div className="left">
+            <Typography mt={2} mb={2} variant="h6" fontWeight="bold">User Name</Typography>
+        </div>
+        <div className="right">
+        <Typography mt={2} mb={2}>
+              <Link to="#"><ToggleOnIcon /></Link>
+              <Link to="#"><EditIcon sx={{ marginLeft: "25px" }} /></Link>
+              <Link to="#"><DeleteIcon sx={{ marginLeft: "25px",marginRight: "25px" }} /></Link>
+            </Typography>
+        </div>
+      </div>
+
+      <div className="userList">
+        <div className="left">
+            <Typography mt={2} mb={2} variant="h6" fontWeight="bold">User Name</Typography>
+        </div>
+        <div className="right">
+        <Typography mt={2} mb={2}>
+              <Link to="#"><ToggleOnIcon /></Link>
+              <Link to="#"><EditIcon sx={{ marginLeft: "25px" }} /></Link>
+              <Link to="#"><DeleteIcon sx={{ marginLeft: "25px",marginRight: "25px" }} /></Link>
+            </Typography>
+        </div>
+      </div>
       </Box>
     </>
   );
